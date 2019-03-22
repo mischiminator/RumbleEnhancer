51059f61feeded0cf38304ece94507222bf9abeb
mischiminator <38972920+mischiminator@users.noreply.github.com>
2019-03-22 02:23:35 +0100

    Rumble Enhancer 1.2.0
    Added ingame settings
    these are the same as in the modprefs.ini file

--- RumbleEnhancer/Plugin.cs
+++ RumbleEnhancer/Plugin.cs
@@ -2,6 +2,12 @@
 using UnityEngine.SceneManagement;
 using System.Linq;
 using UnityEngine;
+using CustomUI;
+using CustomUI.GameplaySettings;
+using CustomUI.Settings;
+using CustomUI.MenuButton;
+using CustomUI.Utilities;
+using System;
 
 namespace RumbleEnhancer
 {
@@ -10,14 +16,17 @@ namespace RumbleEnhancer
 		public const string PluginName = "RumbleEnhancer";
 
 		public string Name => PluginName;
-		public string Version => "1.1.0";
+		public string Version => "1.2.0";
 
-		private readonly string[] GameplaySceneNames = { "Menu", "GameCore" };
+        bool toggleValue = false;
+
+        private readonly string[] GameplaySceneNames = { "Menu", "GameCore" };
 
 		public void OnApplicationStart()
 		{
 			SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
 			SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+            Console.WriteLine("[RumbleEnhancer] Plugin loaded");
 		}
 
 		private void SceneManagerOnActiveSceneChanged(Scene fromScene, Scene toScene)
@@ -37,7 +46,27 @@ namespace RumbleEnhancer
 			SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
 		}
 
-		private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1) { }
+		private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            if (arg0.name == "MenuCore")
+            {
+                SubMenu settingsSubmenu = SettingsUI.CreateSubMenu("Rumble Enhancer");
+
+                IntViewController rumbleStrength = settingsSubmenu.AddInt("Rumble Strength", 0, 10, 1);
+                rumbleStrength.GetValue += delegate { return ModPrefs.GetInt(Name, "RumbleStrength", 1, true); };
+                rumbleStrength.SetValue += delegate (int value) { ModPrefs.SetInt(Name, "RumbleStrength", value); };
+
+                IntViewController rumbleTime = settingsSubmenu.AddInt("Rumble Length\t\t(in ms)", 0, 250, 5);
+                rumbleTime.GetValue += delegate { return ModPrefs.GetInt(Name, "RumbleTimeMS", 25, true); };
+                rumbleTime.SetValue += delegate (int value) { ModPrefs.SetInt(Name, "RumbleTimeMS", value); };
+
+
+                IntViewController rumblePause = settingsSubmenu.AddInt("Rumble Interval\t(in ms)", 0, 250, 1);
+                rumblePause.GetValue += delegate { return ModPrefs.GetInt(Name, "TimeBetweenRumblePulsesMS", 5, true); };
+                rumblePause.SetValue += delegate (int value) { ModPrefs.SetInt(Name, "TimeBetweenRumblePulsesMS", value); };
+
+            }
+        }
 
 		public void OnLevelWasLoaded(int level) { }
 
@@ -51,7 +80,7 @@ namespace RumbleEnhancer
 		{
 			get
 			{
-				int Time = ModPrefs.GetInt(PluginName, "RumbleTimeMS", 250, true);
+				int Time = ModPrefs.GetInt(PluginName, "RumbleTimeMS", 25, true);
 				return Mathf.Clamp(Time, 0, Time);
 			}
 		}
